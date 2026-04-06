2d932e015fc25d5b7b08c9bee9362ef115d152cf
Maxime Martinez <mmartinez27@myges.fr>
2026-04-06 10:56:40 +0200

    modified dataset.py

--- python/dataset.py
+++ python/dataset.py
@@ -8,18 +8,25 @@ def load_dataset(folder_path):
     
     chats_path = os.path.join(folder_path, "chats")
     chiens_path = os.path.join(folder_path, "chiens")
+    autres_path = os.path.join(folder_path, "autres")
     
     for img_name in os.listdir(chats_path):
         img = Image.open(os.path.join(chats_path, img_name))
         img = img.resize((64, 64)).convert("L")
         X.append(np.array(img).flatten() / 255.0)
-        y.append(0)  #0 = chat
+        y.append(0)  # 0 = chat
     
     for img_name in os.listdir(chiens_path):
         img = Image.open(os.path.join(chiens_path, img_name))
         img = img.resize((64, 64)).convert("L")
         X.append(np.array(img).flatten() / 255.0)
-        y.append(1)  #1 = chien
+        y.append(1)  # 1 = chien
+
+    for img_name in os.listdir(autres_path):
+        img = Image.open(os.path.join(autres_path, img_name))
+        img = img.resize((64, 64)).convert("L")
+        X.append(np.array(img).flatten() / 255.0)
+        y.append(2)  # 2 = autre
     
     return np.array(X, dtype=np.float32), np.array(y, dtype=np.float32)
 
@@ -30,3 +37,10 @@ if __name__ == "__main__":
     print(f"Train : {X_train.shape}")
     print(f"Test  : {X_test.shape}")
     print(f"Labels train : {y_train}")
+
+    # mélanger pour biais d'ordre
+    indices = np.random.permutation(len(y_train))
+    X_train = X_train[indices]
+    y_train = y_train[indices]
+
+    print(f"Labels mélangés : {y_train}")
